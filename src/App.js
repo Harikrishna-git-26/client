f4973dd98475348b208fc7dcfc29021d86828b90
Harikrishna-git-26 <tiworkpart2@gmail.com>
2025-10-26 12:30:00 +0530

    "new app js"

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import io from "socket.io-client";
 import Peer from "simple-peer";
 import ReactPlayer from "react-player";
 
-const SIGNALING_SERVER =
-  process.env.REACT_APP_SIGNALING_SERVER || "http://localhost:5000";
+REACT_APP_SIGNALING_SERVER = "https://webrtc-server-wwai.onrender.com";
 
 export default function App() {
   const [name, setName] = useState("");
